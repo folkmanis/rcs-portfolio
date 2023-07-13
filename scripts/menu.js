6bbc9c62574c3b46d9557782bcb7e9bdde15e9ee
folkmanis <folkmanis@gmail.com>
2023-07-13 10:11:59 +0300

    working on animations

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,5 @@
 const BACKDROP_CLASS = 'backdrop';
+const backdropAnimation = [{ opacity: 0.7 }, { opacity: 0 }];
 
 let isOpen = false;
 
@@ -28,7 +29,6 @@ function openMenu() {
 }
 
 function closeMenu() {
-    console.log('close menu');
     removeBackdrop();
     const nav = document.getElementsByTagName('nav')[0];
     if (nav) {
@@ -43,14 +43,22 @@ function closeMenu() {
 
 function createBackdrop() {
     const backdrop = document.createElement('div');
-    backdrop.classList.add(BACKDROP_CLASS);
+    backdrop.classList.add(BACKDROP_CLASS, 'backdrop-visible');
     backdrop.addEventListener('click', closeMenu);
     document.body.appendChild(backdrop);
 }
 
 function removeBackdrop() {
     const backdrop = document.getElementsByClassName(BACKDROP_CLASS)[0];
+    console.log(backdrop);
     if (backdrop) {
-        backdrop.remove();
+        backdrop.addEventListener('animationstart', (event) =>
+            console.log(event)
+        );
+        backdrop.addEventListener('animationend', () =>
+            backdrop.remove()
+        );
+        backdrop.animate(backdropAnimation, 2000);
+        // backdrop.classList.remove('backdrop-visible');
     }
 }
